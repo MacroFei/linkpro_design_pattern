369371afa8b8a8f30c848c17da1ca9dc8801385c
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-01-26 00:57:45 +0800

    demeter2

--- src/main/java/com/xlccc/design/principle/demeter/Boss.java
+++ src/main/java/com/xlccc/design/principle/demeter/Boss.java
@@ -1,14 +1,8 @@
 package com.xlccc.design.principle.demeter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Boss {
     public void commandCheckNumber(TeamLeader teamLeader){
-        List<Course> courseList = new ArrayList<Course>();
-        for (int i = 0 ; i<20 ; i++){
-            courseList.add(new Course());
-        }
-        teamLeader.checkNumberOfCourses(courseList);
+
+        teamLeader.checkNumberOfCourses();
     }
 }
