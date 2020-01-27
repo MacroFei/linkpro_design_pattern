11cf3b542bbe4e1ca974e35a36470c8e065e8fe8
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-01-27 15:02:00 +0800

    factorymethod

---
+++ src/main/java/com/xlccc/design/pattern/creational/factorymethod/FEVideo.java
@@ -0,0 +1,7 @@
+package com.xlccc.design.pattern.creational.factorymethod;
+
+public class FEVideo extends  Video {
+    public void produce() {
+        System.out.println("录制FEVideo视频");
+    }
+}
---
+++ src/main/java/com/xlccc/design/pattern/creational/factorymethod/Video.java
@@ -0,0 +1,5 @@
+package com.xlccc.design.pattern.creational.factorymethod;
+
+public abstract class Video {
+    public abstract  void produce();
+}
